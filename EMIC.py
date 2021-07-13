a0e7f200c3a571e2174935c22dd786285a1e4fae
linken666 <83584884+linken666@users.noreply.github.com>
2021-07-13 15:33:23 +0800

    Update EMIC.py
    在自己電腦上試做的emic爬蟲，可以成功運作
    後續與課程6-2、6-3的範例程式結合修改成佈置到GCP上的版本

--- EMIC.py
+++ EMIC.py
@@ -7,17 +7,24 @@ Created on Mon May 24 10:38:32 2021
 
 import xml.etree.ElementTree as ET
 import pandas as pd 
+import requests as re  
 
+#將網站上的emic資料複製下來存到自己的電腦上
+url="https://portal2.emic.gov.tw/Pub/DIM2/OpenData/Disaster.xml"
+resp = re.get(url, headers = {
+        "user-agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36" 
+    })
+resp.encoding = 'utf-8-sig'
+f = open('emic2.0.xml' , 'w' , encoding = 'utf-8-sig')
+f.write(resp.text)
+
+#將需要的資料轉成csv檔
 xmlparse = ET.parse('emic.xml')
 root = xmlparse.getroot()
-
-cols = ["CASE_ID", "CASE_DT", "CASE_LOC", "DISASTER_MAIN_TYPE", "CASE_DESCRIPTION"]
 rows = []
-
 print(root[1].tag)
 
 cnt = 0
-
 for i in root[1].findall('{http://emic.eoc.gov.tw/disaster_info}DISASTER_DATA'):
     print(cnt)
     cnt += 1
@@ -25,7 +32,7 @@ for i in root[1].findall('{http://emic.eoc.gov.tw/disaster_info}DISASTER_DATA'):
     CASE_DT = i.find("{http://emic.eoc.gov.tw/disaster_info}CASE_DT").text
     CASE_LOC = i.find("{http://emic.eoc.gov.tw/disaster_info}CASE_LOC").text
     DISASTER_MAIN_TYPE_NODE = i.find("{http://emic.eoc.gov.tw/disaster_info}DISASTER_MAIN_TYPE")
-    DISASTER_MAIN_TYPE = "QQ" if DISASTER_MAIN_TYPE_NODE is None  else DISASTER_MAIN_TYPE_NODE.text
+    DISASTER_MAIN_TYPE = "None" if DISASTER_MAIN_TYPE_NODE is None  else DISASTER_MAIN_TYPE_NODE.text
     CASE_DESCRIPTION = i.find("{http://emic.eoc.gov.tw/disaster_info}CASE_DESCRIPTION").text
     print(CASE_ID)
     
@@ -39,8 +46,3 @@ for i in root[1].findall('{http://emic.eoc.gov.tw/disaster_info}DISASTER_DATA'):
 df = pd.DataFrame(rows)
 df.to_csv('output.csv' , encoding = 'utf-8-sig')
 
-import csv
-#python2可以用file替代open
-with open("test.csv","w") as csvfile: 
- writer = csv.writer( csvfile )
- writer.writerows( rows )
