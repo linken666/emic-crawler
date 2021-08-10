9e45a1998419a24721e99bf2bffdafef749b5b7b
linken666 <83584884+linken666@users.noreply.github.com>
2021-08-10 11:55:27 +0800

    Update emic-writer.py

--- emic-writer.py
+++ emic-writer.py
@@ -42,19 +42,19 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
 
 #開啟先前上傳的檔案
 def main(event, context):
-  download_blob(event['bucket'], event['name'], '/tmp/'+event['name'])
-  filename = f"{event['name']}.csv"
-  pat = '/tmp/'+event['name']
-  _emic_csv_writer(pat , filename)
-  upload_blob("emic-csv-cla", "/tmp/"+filename, filename)
+  download_blob(event['bucket'], event['name'], '/tmp/'+event['name'])  #下載新增的xml檔
+  filename = f"{event['name']}.csv"  #開一個csv
+  pat = '/tmp/'+event['name']  #xml檔的路徑
+  _emic_csv_writer(pat , filename)  #回傳給我csv
+  upload_blob("emic-csv-cla", "/tmp/"+filename, filename)  #上傳到值區
 
 #將該檔案解析後儲存為csv檔
 def _emic_csv_writer(pat , filename):
-  xmlparse = ET.parse( pat )
-  root = xmlparse.getroot()
+  xmlparse = ET.parse( pat )  #解析路徑的xml
+  root = xmlparse.getroot()  
   rows = []
   cnt = 0
-
+#以下開始解析(這段之前在電腦上可以用)
   for i in root[1].findall('{http://emic.eoc.gov.tw/disaster_info}DISASTER_DATA'):
       print(cnt)
       cnt += 1
@@ -74,5 +74,5 @@ def _emic_csv_writer(pat , filename):
                    "CASE_DESCRIPTION": CASE_DESCRIPTION
                    })
     
-  df = pd.DataFrame(rows)
-  df.to_csv( filename , encoding = 'utf-8')
+  df = pd.DataFrame(rows)  #把rows做成DataFrame(像excel的那種表格)
+  df.to_csv( filename , encoding = 'utf-8')  #把df輸出成csv檔
