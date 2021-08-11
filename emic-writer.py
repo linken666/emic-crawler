ef8db65a763ca46915834d46bf73e6b8c20b8882
linken666 <83584884+linken666@users.noreply.github.com>
2021-08-11 17:05:59 +0800

    Update emic-writer.py
    把一些除錯用的print先留著
    以後可以學著除錯

--- emic-writer.py
+++ emic-writer.py
@@ -1,5 +1,5 @@
 import xml.etree.ElementTree as ET
-import pandas as pd 
+import pandas as pd
 
 from google.cloud import storage
 
@@ -16,11 +16,11 @@ def download_blob(bucket_name, source_blob_name, destination_file_name):
     blob = bucket.blob(source_blob_name)
     blob.download_to_filename(destination_file_name)
 
-    print(
-        "Blob {} downloaded to {}.".format(
-            source_blob_name, destination_file_name
-        )
-    )
+    #print(
+    #    "Blob {} downloaded to {}.".format(
+    #        source_blob_name, destination_file_name
+    #    )
+    #)
 
 def upload_blob(bucket_name, source_file_name, destination_blob_name):
     """Uploads a file to the bucket."""
@@ -34,17 +34,19 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
 
     blob.upload_from_filename(source_file_name)
 
-    print(
-        "File {} uploaded to {}.".format(
-            source_file_name, destination_blob_name
-        )
-    )
+    #print(
+    #    "File {} uploaded to {}.".format(
+    #        source_file_name, destination_blob_name
+    #    )
+    #)
 
 #開啟先前上傳的檔案
 def main(event, context):
   download_blob(event['bucket'], event['name'], '/tmp/'+event['name'])  #下載新增的xml檔
   filename = f"{event['name']}.csv"  #開一個空的csv
+  #print("filename:",filename)
   pat = '/tmp/'+event['name']  #xml檔的路徑
+  #print("pat:",pat)
   _emic_csv_writer(pat , filename)  #回傳給我寫好的csv
   upload_blob("emic-csv-cla", "/tmp/"+filename, filename)  #上傳到值區
 
@@ -56,7 +58,7 @@ def _emic_csv_writer(pat , filename):
   cnt = 0
 #以下開始解析(這段之前在電腦上可以用)
   for i in root[1].findall('{http://emic.eoc.gov.tw/disaster_info}DISASTER_DATA'):
-      print(cnt)
+      #print(cnt)
       cnt += 1
       CASE_ID = i.find("{http://emic.eoc.gov.tw/disaster_info}CASE_ID").text
       CASE_DT = i.find("{http://emic.eoc.gov.tw/disaster_info}CASE_DT").text
@@ -64,7 +66,7 @@ def _emic_csv_writer(pat , filename):
       DISASTER_MAIN_TYPE_NODE = i.find("{http://emic.eoc.gov.tw/disaster_info}DISASTER_MAIN_TYPE")
       DISASTER_MAIN_TYPE = "None" if DISASTER_MAIN_TYPE_NODE is None  else DISASTER_MAIN_TYPE_NODE.text
       CASE_DESCRIPTION = i.find("{http://emic.eoc.gov.tw/disaster_info}CASE_DESCRIPTION").text
-      print(CASE_ID)
+      #print(CASE_ID)
     
     
       rows.append({"CASE_ID": CASE_ID,
@@ -73,6 +75,8 @@ def _emic_csv_writer(pat , filename):
                    "DISASTER_MAIN_TYPE": DISASTER_MAIN_TYPE,
                    "CASE_DESCRIPTION": CASE_DESCRIPTION
                    })
-    
+  #print(rows)  
   df = pd.DataFrame(rows)  #把rows做成DataFrame(像excel的那種表格)
-  df.to_csv( filename , encoding = 'utf-8')  #把df輸出成csv檔
+  tempdes="/tmp/"+filename+".csv"
+  #print("des:",tempdes)
+  df.to_csv(tempdes, encoding = 'utf_8_sig')  #把df輸出成csv檔，utf-8、utf_8都會亂碼，反正就這個邏輯每個都試試
